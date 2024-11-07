2f72f09f2d8636661195aaaf2c1f65b9eced6b45
Aymen Assaf <aymen.assaf@edu.uca.ma>
2024-11-08 00:54:53 +0100

    norms :/

--- execution.c
+++ execution.c
@@ -6,7 +6,7 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 19:40:26 by aassaf            #+#    #+#             */
-/*   Updated: 2024/11/07 23:42:32 by aassaf           ###   ########.fr       */
+/*   Updated: 2024/11/08 00:50:40 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,61 +75,76 @@ int keyDown(t_data *data, int keycode)
 	return data->keys[keycode];
 }
 
+static void hdl_up_down(t_data *data, double *newPosX, double *newPosY)
+{
+    double moveStep = data->move_speed * 3.0;
+
+    if (keyDown(data, KEY_UP) || keyDown(data, KEY_W))
+	{
+        *newPosX += data->dir_x * moveStep;
+        *newPosY += data->dir_y * moveStep;
+    }
+    if (keyDown(data, KEY_DOWN) || keyDown(data, KEY_S))
+	{
+        *newPosX -= data->dir_x * moveStep;
+        *newPosY -= data->dir_y * moveStep;
+    }
+}
+
+static void hdl_strafe(t_data *data, double *newPosX, double *newPosY)
+{
+    double strafeStep;
+	strafeStep = data->move_speed * 3.0;
+
+    if (keyDown(data, KEY_D))
+	{
+        *newPosX -= data->plane_x * strafeStep;
+        *newPosY -= data->plane_y * strafeStep;
+    }
+    if (keyDown(data, KEY_A))
+	{
+        *newPosX += data->plane_x * strafeStep;
+        *newPosY += data->plane_y * strafeStep;
+    }
+}
+
+static void hdl_r_rot(t_data *data)
+{
+    double oldDirX = data->dir_x;
+    data->dir_x = data->dir_x * cos(-data->rot_speed) - data->dir_y * sin(-data->rot_speed);
+    data->dir_y = oldDirX * sin(-data->rot_speed) + data->dir_y * cos(-data->rot_speed);
+    double oldPlaneX = data->plane_x;
+    data->plane_x = data->plane_x * cos(-data->rot_speed) - data->plane_y * sin(-data->rot_speed);
+    data->plane_y = oldPlaneX * sin(-data->rot_speed) + data->plane_y * cos(-data->rot_speed);
+}
+
+static void hdl_l_rot(t_data *data)
+{
+    double oldDirX = data->dir_x;
+    data->dir_x = data->dir_x * cos(data->rot_speed) - data->dir_y * sin(data->rot_speed);
+    data->dir_y = oldDirX * sin(data->rot_speed) + data->dir_y * cos(data->rot_speed);
+    double oldPlaneX = data->plane_x;
+    data->plane_x = data->plane_x * cos(data->rot_speed) - data->plane_y * sin(data->rot_speed);
+    data->plane_y = oldPlaneX * sin(data->rot_speed) + data->plane_y * cos(data->rot_speed);
+}
+
 void read_keys(myvar *var)
 {
-
-	t_data *data = var->data;
-	// print_map(var->s);
-    double moveStep = data->move_speed * 3.0;
-	double strafeStep = data->move_speed * 3.0;
+    t_data *data = var->data;
     double newPosX = data->pos_x;
     double newPosY = data->pos_y;
 
-    if (keyDown(data, KEY_UP) || keyDown(data, KEY_W))
-    {
-        newPosX += data->dir_x * moveStep;
-        newPosY += data->dir_y * moveStep;
-    }
-    if (keyDown(data, KEY_DOWN) || keyDown(data, KEY_S))
-    {
-        newPosX -= data->dir_x * moveStep;
-        newPosY -= data->dir_y * moveStep;
-    }
-	if(keyDown(data, KEY_D))
-	{
-		newPosX -= data->plane_x * strafeStep;
-		newPosY -= data->plane_y * strafeStep;
-	}
-	if(keyDown(data, KEY_A))
-	{
-		newPosX += data->plane_x * strafeStep;
-		newPosY += data->plane_y * strafeStep;
-	}
+    hdl_up_down(data, &newPosX, &newPosY);
+    hdl_strafe(data, &newPosX, &newPosY);
     if (is_valid_position(var, newPosX, data->pos_y))
         data->pos_x = newPosX;
     if (is_valid_position(var, data->pos_x, newPosY))
         data->pos_y = newPosY;
     if (keyDown(data, KEY_RIGHT))
-    {
-        double oldDirX = data->dir_x;
-        data->dir_x = data->dir_x * cos(-data->rot_speed) - data->dir_y * sin(-data->rot_speed);
-        data->dir_y = oldDirX * sin(-data->rot_speed) + data->dir_y * cos(-data->rot_speed);
-        double oldPlaneX = data->plane_x;
-        data->plane_x = data->plane_x * cos(-data->rot_speed) - data->plane_y * sin(-data->rot_speed);
-        data->plane_y = oldPlaneX * sin(-data->rot_speed) + data->plane_y * cos(-data->rot_speed);
-    }
+        hdl_r_rot(data);
     if (keyDown(data, KEY_LEFT))
-    {
-        double oldDirX = data->dir_x;
-        data->dir_x = data->dir_x * cos(data->rot_speed) - data->dir_y * sin(data->rot_speed);
-        data->dir_y = oldDirX * sin(data->rot_speed) + data->dir_y * cos(data->rot_speed);
-        double oldPlaneX = data->plane_x;
-        data->plane_x = data->plane_x * cos(data->rot_speed) - data->plane_y * sin(data->rot_speed);
-        data->plane_y = oldPlaneX * sin(data->rot_speed) + data->plane_y * cos(data->rot_speed);
-    }
-
-    if (keyDown(data, KEY_ESC))
-    {
+        hdl_l_rot(data);
+    if (keyDown(data, KEY_ESC)) {
         mlx_destroy_window(data->mlx, data->win);
         exit(0);
     }
@@ -223,58 +238,66 @@ void	calcul_wall_dist(t_data *data, t_ray *ray, double ray_dir_x, double ray_dir
 		ray->draw_end = screen_height - 1;
 
 }
-void print_map(char **s)
+
+static void init_N(myvar *var)
 {
-	int i = 0;
-	while (s[i])
-	{
-		printf("%s\n", s[i]);
-		i++;
-	}
+    var->data->dir_x = 0;
+    var->data->dir_y = -1;
+    var->data->plane_x = 0.66;
+    var->data->plane_y = 0;
+}
+
+static void init_S(myvar *var)
+{
+    var->data->dir_x = 0;
+    var->data->dir_y = 1;
+    var->data->plane_x = -0.66;
+    var->data->plane_y = 0;
+}
+
+static void init_E(myvar *var)
+{
+    var->data->dir_x = 1;
+    var->data->dir_y = 0;
+    var->data->plane_x = 0;
+    var->data->plane_y = 0.66;
+}
+
+static void init_W(myvar *var)
+{
+    var->data->dir_x = -1;
+    var->data->dir_y = 0;
+    var->data->plane_x = 0;
+    var->data->plane_y = -0.66;
+}
+
+static void set_direc_player(myvar *var, char direc)
+{
+	if (direc == 'N')
+		init_N(var);
+	else if (direc == 'S')
+		init_S(var);
+	else if (direc == 'E')
+		init_E(var);
+	else if (direc == 'W')
+		init_W(var);
 }
 
 void detect_direc_player(myvar *var)
 {
-    int i = 0;
-    int j = 0;
+	int(i), (j);
+
+	i = 0;
     while (var->s[i])
     {
         j = 0;
         while (var->s[i][j])
         {
-            if (var->s[i][j] == 'N' || var->s[i][j] == 'S' || var->s[i][j] == 'E' || var->s[i][j] == 'W')
-            {
+            if (strchr("NSEW", var->s[i][j]))
+			{
                 var->player.x = j;
                 var->player.y = i;
-                if (var->s[i][j] == 'N')
-                {
-                	printf("Found player at (%d,  %d): %c\n", i, j, var->s[i][j]);
-                    var->data->dir_x = 0;
-                    var->data->dir_y = -1;
-                    var->data->plane_x = 0.66;
-                    var->data->plane_y = 0;
-                }
-                else if (var->s[i][j] == 'S')
-                {
-                    var->data->dir_x = 0;
-                    var->data->dir_y = 1;
-                    var->data->plane_x = -0.66;
-                    var->data->plane_y = 0;
-                }
-                else if (var->s[i][j] == 'E')
-                {
-                    var->data->dir_x = 1;
-                    var->data->dir_y = 0;
-                    var->data->plane_x = 0;
-                    var->data->plane_y = 0.66;
-                }
-                else if (var->s[i][j] == 'W')
-                {
-                    var->data->dir_x = -1;
-                    var->data->dir_y = 0;
-                    var->data->plane_x = 0;
-                    var->data->plane_y = -0.66;
-                }
+                set_direc_player(var, var->s[i][j]);
                 var->s[i][j] = '0';
                 return;
             }
@@ -327,106 +350,179 @@ int get_2d_map_color(int wall_type, int orientation)
     return 0x000000;             // Ground - Black
 }
 
-void draw_2d_map(myvar *var)
+static int get_wall_orientation(myvar *var, int x, int y)
 {
-    t_data *data = var->data;
-    int x, y;
-    int mini_size = MAP_WIDTH / 20;
-    int map_offset = 10;
-    y = 0;
-    while (y < var->data->map_rows)
-    {
-        x = 0;
-        while (x < var->data->map_cols)
-        {
-            if ((size_t)x < ft_strlen(var->s[y]))
-            {
-                int wall_type = var->s[y][x] - '0';
-                int orientation = -1;
-                if (wall_type == 1)
-                {
-                    if (y > 0 && (size_t)x < ft_strlen(var->s[y-1]) && var->s[y-1][x] == '0')
-                        orientation = 1;
-                    else if (y < var->data->map_rows - 1 && (size_t)x < ft_strlen(var->s[y+1]) && var->s[y+1][x] == '0')
-                        orientation = 0;
-                    else if (x > 0 && var->s[y][x-1] == '0')
-                        orientation = 2;
-                    else if (x < var->data->map_cols - 1 && var->s[y][x+1] == '0')
-                        orientation = 3;
-                }
+    if (y > 0 && (size_t)x < ft_strlen(var->s[y-1]) && var->s[y-1][x] == '0')
+        return 1;
+    if (y < var->data->map_rows - 1 && (size_t)x < ft_strlen(var->s[y+1]) && var->s[y+1][x] == '0')
+        return 0;
+    if (x > 0 && var->s[y][x-1] == '0')
+        return 2;
+    if (x < var->data->map_cols - 1 && var->s[y][x+1] == '0')
+        return 3;
+    return -1;
+}
 
-                int color = get_2d_map_color(wall_type, orientation);
+static void draw_map_tile(myvar *var, int x, int y, int mini_size, int map_offset)
+{
+    int wall_type;
+    int orientation;
+    int color;
+	int py;
+	int px;
 
-                int py = 0;
-                while (py < mini_size)
-                {
-                    int px = 0;
-                    while (px < mini_size)
-                    {
-                        my_mlx_pixel_put(data,
-                                        map_offset + x * mini_size + px,
-                                        map_offset + y * mini_size + py,
-                                        color);
-                        px++;
-                    }
-                    py++;
-                }
-            }
-            x++;
-        }
-        y++;
-    }
+    if ((size_t)x >= ft_strlen(var->s[y]))
+		return;
+	wall_type = var->s[y][x] - '0';
+	if (wall_type == 1)
+		orientation = get_wall_orientation(var, x, y);
+	else
+		orientation = -1;
+	color = get_2d_map_color(wall_type, orientation);
+
+	py = 0;
+	while (py < mini_size)
+	{
+		px = 0;
+		while (px < mini_size)
+		{
+			my_mlx_pixel_put(var->data,
+			map_offset + x * mini_size + px, map_offset + y * mini_size + py, color);
+			px++;
+		}
+		py++;
+	}
+}
+
+static void draw_player_position(t_data *data, int mini_size, int map_offset)
+{
     int player_x = (int)(data->pos_x * mini_size);
     int player_y = (int)(data->pos_y * mini_size);
-    int px = -2;
-    while (px <= 2)
+
+	int px = -2;
+	while (px <= 2) {
+		int py = -2;
+		while (py <= 2) {
+			my_mlx_pixel_put(data,
+							map_offset + player_x + px,
+							map_offset + player_y + py,
+							0xFF0000);
+			py++;
+		}
+		px++;
+	}
+}
+
+void draw_2d_map(myvar *var)
+{
+    int mini_size;
+    int map_offset;
+
+	mini_size = MAP_WIDTH / 20;
+	map_offset = 10;
+	int y;
+	int x;
+
+	y = 0;
+
+	while (y < var->data->map_rows)
+	{
+		x = 0;
+		while (x < var->data->map_cols)
+		{
+			draw_map_tile(var, x, y, mini_size, map_offset);
+			x++;
+		}
+		y++;
+	}
+    draw_player_position(var->data, mini_size, map_offset);
+}
+
+static void calculate_step_direction(double ray_dir_x, double ray_dir_y, int *step_x, int *step_y)
+{
+    if (ray_dir_x < 0)
+        *step_x = -1;
+    else
+        *step_x = 1;
+
+    if (ray_dir_y < 0)
+        *step_y = -1;
+    else
+        *step_y = 1;
+}
+
+static void calculate_side_distances(double ray_pos_x, double ray_pos_y,
+                                   double delta_dist_x, double delta_dist_y,
+                                   int step_x, int step_y,
+                                   double *side_dist_x, double *side_dist_y)
+{
+    if (step_x < 0)
+        *side_dist_x = (ray_pos_x - floor(ray_pos_x)) * delta_dist_x;
+    else
+        *side_dist_x = (ceil(ray_pos_x) - ray_pos_x) * delta_dist_x;
+
+    if (step_y < 0)
+        *side_dist_y = (ray_pos_y - floor(ray_pos_y)) * delta_dist_y;
+    else
+        *side_dist_y = (ceil(ray_pos_y) - ray_pos_y) * delta_dist_y;
+}
+
+static void draw_ray_pixel(t_data *data, double ray_pos_x, double ray_pos_y,
+                          int tile_size, int color)
+{
+    int pixel_x;
+	pixel_x = (int)(ray_pos_x * tile_size);
+    int pixel_y;
+	pixel_y = (int)(ray_pos_y * tile_size);
+
+    if (pixel_x >= 0 && pixel_x < MAP_WIDTH &&
+        pixel_y >= 0 && pixel_y < MAP_WIDTH)
+        my_mlx_pixel_put(data, pixel_x, pixel_y, color);
+}
+
+static void update_ray_position(double *ray_pos_x, double *ray_pos_y,
+                              double *side_dist_x, double *side_dist_y,
+                              double delta_dist_x, double delta_dist_y,
+                              int step_x, int step_y)
+{
+    if (*side_dist_x < *side_dist_y)
     {
-        int py = -2;
-        while (py <= 2)
-        {
-            my_mlx_pixel_put(data, map_offset + player_x + px, map_offset + player_y + py, 0xFF0000);
-            py++;
-        }
-        px++;
+        *side_dist_x += delta_dist_x;
+        *ray_pos_x += step_x;
+    }
+    else
+    {
+        *side_dist_y += delta_dist_y;
+        *ray_pos_y += step_y;
     }
 }
 
 void draw_ray(myvar *var, t_data *data, double ray_dir_x, double ray_dir_y, int color)
 {
-	int tile_size = MAP_WIDTH / 20;
-	double ray_pos_x = data->pos_x;
-	double ray_pos_y = data->pos_y;
+    int tile_size = MAP_WIDTH / 20;
+    double ray_pos_x = data->pos_x;
+    double ray_pos_y = data->pos_y;
+    double delta_dist_x = fabs(1 / ray_dir_x);
+    double delta_dist_y = fabs(1 / ray_dir_y);
+    int step_x, step_y;
+    calculate_step_direction(ray_dir_x, ray_dir_y, &step_x, &step_y);
 
-	double delta_dist_x = fabs(1 / ray_dir_x);
-	double delta_dist_y = fabs(1 / ray_dir_y);
-
-	int step_x = (ray_dir_x < 0) ? -1 : 1;
-	int step_y = (ray_dir_y < 0) ? -1 : 1;
-
-	double side_dist_x = (step_x < 0) ? (ray_pos_x - floor(ray_pos_x)) * delta_dist_x : (ceil(ray_pos_x) - ray_pos_x) * delta_dist_x;
-	double side_dist_y = (step_y < 0) ? (ray_pos_y - floor(ray_pos_y)) * delta_dist_y : (ceil(ray_pos_y) - ray_pos_y) * delta_dist_y;
-
-	int hit = 0;
-	while (!hit)
-	{
-		int pixel_x = (int)(ray_pos_x * tile_size);
-		int pixel_y = (int)(ray_pos_y * tile_size);
-
-		if (pixel_x >= 0 && pixel_x < MAP_WIDTH && pixel_y >= 0 && pixel_y < MAP_WIDTH)
-			my_mlx_pixel_put(data, pixel_x, pixel_y, color);
-		if (side_dist_x < side_dist_y)
-		{
-			side_dist_x += delta_dist_x;
-			ray_pos_x += step_x;
-		}
-		else
-		{
-			side_dist_y += delta_dist_y;
-			ray_pos_y += step_y;
-		}
-		if (is_wall(var, (int)ray_pos_x, (int)ray_pos_y))
-			hit = 1;
-	}
+    double side_dist_x, side_dist_y;
+    calculate_side_distances(ray_pos_x, ray_pos_y,
+                           delta_dist_x, delta_dist_y,
+                           step_x, step_y,
+                           &side_dist_x, &side_dist_y);
+    int hit = 0;
+    while (!hit)
+    {
+        draw_ray_pixel(data, ray_pos_x, ray_pos_y, tile_size, color);
+        update_ray_position(&ray_pos_x, &ray_pos_y,
+                          &side_dist_x, &side_dist_y,
+                          delta_dist_x, delta_dist_y,
+                          step_x, step_y);
+        if (is_wall(var, (int)ray_pos_x, (int)ray_pos_y))
+            hit = 1;
+    }
 }
 
 void calcul_map_dimens(myvar *var)
@@ -446,7 +542,6 @@ void calcul_map_dimens(myvar *var)
     }
 	var->data->map_rows = row;
 	var->data->map_cols = col;
-	printf("rows %d cols %d\n",var->data->map_rows,var->data->map_cols);
 }
 int raycasting_loop(myvar *var)
 {
@@ -463,7 +558,6 @@ int raycasting_loop(myvar *var)
 	data->move_speed = frame_time * 2.0;
 	data->rot_speed = frame_time * 3.0;
 	read_keys(var);
-	// print_player_position(data);
 	memset(data->addr, 0, screen_width * screen_height * (data->bits_per_pixel / 8));
 	x = 0;
 	while (x < screen_width)
@@ -474,7 +568,6 @@ int raycasting_loop(myvar *var)
 		calculate_dist(data, &ray, ray_dir_x, ray_dir_y);
 		dda_algo(&ray, var);
 		calcul_wall_dist(data, &ray, ray_dir_x, ray_dir_y);
-		// var->s[var->player.x][var->player.y] = '0';
 		draw_v_line(data, x, &ray, var);
 		x++;
 	}
@@ -501,5 +594,4 @@ void execute(myvar *var)
 	mlx_hook(data->win, 17, 0, close_window, data);
 	mlx_loop_hook(data->mlx, raycasting_loop, var);
 	mlx_loop(data->mlx);
-	// print_player_position(data);
 }
